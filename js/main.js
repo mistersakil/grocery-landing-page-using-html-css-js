6000623c99202fc8439f70a72f472b1b8514b56f
Sakil Jomadder <sakilorb@icloud.com>
2021-07-19 22:36:14 +0600

    activated countdown for dealOfTheDay section

--- js/main.js
+++ js/main.js
@@ -31,3 +31,34 @@ navbar.onclick = function (event) {
   }
 };
 /* End: Navbar */
+
+/* Count Down */
+let days = document.querySelector("#days");
+let hours = document.querySelector("#hours");
+let minutes = document.querySelector("#minutes");
+let seconds = document.querySelector("#seconds");
+let futureDate = new Date(2024, 01, 01, 00, 00, 00, 00);
+
+const second = 1000;
+const minute = 60 * second;
+const hour = 60 * minute;
+const day = 24 * hour;
+let dayText, hourText, minuteText, secondText;
+
+const countDown = () => {
+  const currentDate = new Date();
+  const remainingTime = futureDate - currentDate;
+
+  dayText = Math.floor(remainingTime / day);
+  hourText = Math.floor((remainingTime % day) / hour);
+  minuteText = Math.floor((remainingTime % hour) / minute);
+  secondText = Math.ceil((remainingTime % minute) / second);
+
+  days.innerText = dayText <= 0 ? `0${dayText}` : dayText;
+  hours.innerText = hourText <= 0 ? `0${hourText}` : hourText;
+  minutes.innerText = minuteText <= 0 ? `0${minuteText}` : minuteText;
+  seconds.innerText = secondText <= 0 ? `0${secondText}` : secondText;
+};
+setInterval(() => countDown(), 1000);
+
+/* End: Count Down */
